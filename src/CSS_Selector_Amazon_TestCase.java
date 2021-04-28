1d22f25537982354c3f7d9f88c7c88cc5b76dd21
milad103 <melky2008@gmail.com>
2021-04-28 00:53:26 -0400

    Google Search

--- src/CSS_Selector_Amazon_TestCase.java
+++ src/CSS_Selector_Amazon_TestCase.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CSS_Selector_Amazon_TestCase {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
 
 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Milad\\IdeaProjects\\Selenium_Amazon\\Driver\\chromedriver.exe");
@@ -12,8 +12,8 @@ System.setProperty("webdriver.chrome.driver", "C:\\Users\\Milad\\IdeaProjects\\S
 WebDriver driver = new ChromeDriver();
 
 driver.get("https://amazon.com");
-Thread.sleep(6000);
 
-driver.findElement(By.cssSelector("//*[@id=\"navFooter\"]/div[4]/table/tbody/tr[11]/td[11]/a"));
+
+
     }
 }
---
+++ src/GoogleSearch.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleSearch {
+
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Milad\\IdeaProjects\\Selenium_Amazon\\Driver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@title='Search']")));
+        driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("www.apple.com");
+        driver.findElement(By.cssSelector("input[jsaction='trigger.kWlxhc']")).click();
+    }
+}
